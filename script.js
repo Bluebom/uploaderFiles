f56a56436dd1a49c86f7103a51aa30e97f6bb306
franklin <franklinhpf@hotmail.com>
2022-07-16 02:10:12 -0300

    inclusao view 60%

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const wrap = [],
     fileInput = [],
     progressArea = [],
     uplodedArea = [];
+
 for(let i = 1; i < 7; i++){
     wrap.push(document.querySelector(`#wrap0${i}`))
     form.push(wrap[i-1].querySelector(`form`));
@@ -14,31 +15,21 @@ for(let i = 1; i < 7; i++){
     form[i-1].addEventListener("click", () => {
         fileInput[i-1].click();
     })
-    fileInput[i-1].onchange = ({target}) => {
-        let file = target.files[0];
-        if(file) {
-            
-            updateThumbnail(form[i-1], file);
-            let fileName = file.name;
-            let identificador = form[i-1].getAttribute('identificador'); 
-            if(fileName.length >= 12){
-                let splitName = fileName.split('.');
-                fileName = splitName[0].substring(0,12) + `... .${splitName[(parseInt(splitName.length) - 1)]}`;
-            }            
-            fileName = fileName.replace(/(\/)/g,'');
-            let nameUpload = `${identificador}!${fileName}`;
-            uploadFile(fileName, nameUpload, file, progressArea[i-1], uplodedArea[i-1]);
-        }
-    }
+
+    /** ao arrastar arquivos para frente do elemento */
     form[i-1].addEventListener("dragover", (e) => {
         e.preventDefault();
         form[i-1].style.borderStyle = 'solid';
     });
+
+    /** ao arrastar para fora do elemento */
     ["dragleave", "dragend"].forEach((type) => {
         form[i-1].addEventListener(type, (e) => {
           form[i-1].style.borderStyle = 'dotted';
         });
     });
+
+    /** ao soltar o aquivo no elemento (captura do arquivo) */
     form[i-1].addEventListener("drop", (e) => {
         e.preventDefault();
     
@@ -48,21 +39,40 @@ for(let i = 1; i < 7; i++){
         
             form[i-1].style.borderStyle = 'solid';
             let fileName = file.name;
-            let identificador = form[i-1].getAttribute('identificador'); 
             if(fileName.length >= 12){
                 let splitName = fileName.split('.');
                 fileName = splitName[0].substring(0,12) + `... .${splitName[(parseInt(splitName.length) - 1)]}`;
-            }            
+            }
             fileName = fileName.replace(/(\/)/g,'');
-            let nameUpload = `${identificador}!${fileName}`;
+            let identificador = fileInput[i-1].getAttribute('name');
+            let nameUpload = `${identificador}`;
             uploadFile(fileName, nameUpload, file, progressArea[i-1], uplodedArea[i-1]);
         }
     });
+
+    /** Ao selecionar um arquivo no input file */
+    fileInput[i-1].onchange = ({target}) => {
+        let file = target.files[0];
+        if(file) {
+
+            updateThumbnail(form[i-1], file);
+            let fileName = file.name;
+            if(fileName.length >= 12){
+                let splitName = fileName.split('.');
+                fileName = splitName[0].substring(0,12) + `... .${splitName[(parseInt(splitName.length) - 1)]}`;
+            }
+            fileName = fileName.replace(/(\/)/g,'');
+            let identificador = fileInput[i-1].getAttribute('name');
+            let nameUpload = `${identificador}`;
+            uploadFile(fileName, nameUpload, file, progressArea[i-1], uplodedArea[i-1]);
+        }
+    }
 }
 
+/** Atualiza a Thumb para mostra uma miniatura da imagem ou indicar pdf */
 function updateThumbnail(form,file){
-    form.querySelector('.fa-cloud-upload').style.display = 'none';
-    if(file.type != 'application/pdf'){
+    form.querySelector('.bi-cloud-arrow-up').style.display = 'none';
+    if(file.type !== 'application/pdf'){
         form.style.backgroundImage = `url('${URL.createObjectURL(file)}')`
     }else{
         form.style.backgroundImage = `url('php/files/pdf.webp')`;
@@ -70,19 +80,18 @@ function updateThumbnail(form,file){
     }
 }
 
-
+/** Upload de aquivo com a classe XMLHttpRequest */
 function uploadFile(fileName, nameUpload, file, progressArea, uplodedArea){
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "php/upload.php");
   xhr.upload.addEventListener("progress", ({loaded, total}) =>{
-            console.log(loaded,total);
             let fileLoaded = Math.floor((loaded/total)*100);
             let fileTotal = Math.floor(total/1000);
             let fileSize;
             (fileSize < 1024**2) ? fileSize = `${fileTotal} KB` : fileSize = (loaded / (1024**2)).toFixed(2) + " MB";
             let progressHtml = `
             <li class="linha">
-                <i class="fa fa-file-image-o" aria-hidden="true"></i>
+                <i class="bi-card-image"></i>
                 <div class="conteudo">
                     <div class="details">
                         <span class="name">${fileName}</span>
@@ -97,20 +106,20 @@ function uploadFile(fileName, nameUpload, file, progressArea, uplodedArea){
             uplodedArea.style.position = 'absolute';
             progressArea.style.position = 'static';
             progressArea.innerHTML = progressHtml;
-            if(loaded == total){
+            if(loaded === total){
                 progressArea.innerHTML = '';
                 progressArea.style.position = 'absolute';
                 uplodedArea.style.position = 'static';
                 let uploadedHtml = `
                 <li class="linha">
                     <div class="conteudo">
-                        <i class="fa fa-file-image-o" aria-hidden="true"></i>
+                        <i class="bi-card-image"></i>
                         <div class="details">
                             <span class="name">${fileName}</span>
                             <span class="size">${fileSize}</span>
                         </div>
                     </div>
-                    <i class="fa fa-check" aria-hidden="true"></i>
+                    <i class="fs-4 bi-cloud-check"></i>
                 </li>
                 `;
                 uplodedArea.innerHTML = uploadedHtml;
@@ -122,7 +131,7 @@ function uploadFile(fileName, nameUpload, file, progressArea, uplodedArea){
 }
 
 
-// Upload file
+/** Upload de aquivo com o cliente Http Axios */
 function uploadFile2(fileName, nameUpload, file, progressArea, uplodedArea){
     let formData = new FormData();
     formData.append('file',file, nameUpload);
@@ -134,7 +143,6 @@ function uploadFile2(fileName, nameUpload, file, progressArea, uplodedArea){
     }
     let result = axios.post("php/upload.php",formData, {
         onUploadProgress: ({loaded,total}) => {
-            console.log(loaded,total);
             let fileLoaded = Math.floor((loaded/total)*100);
             let fileTotal = Math.floor(total/1000);
             let fileSize;
@@ -156,7 +164,7 @@ function uploadFile2(fileName, nameUpload, file, progressArea, uplodedArea){
             uplodedArea.style.position = 'absolute';
             progressArea.style.position = 'static';
             progressArea.innerHTML = progressHtml;
-            if(loaded == total){
+            if(loaded === total){
                 progressArea.innerHTML = '';
                 progressArea.style.position = 'absolute';
                 uplodedArea.style.position = 'static';
